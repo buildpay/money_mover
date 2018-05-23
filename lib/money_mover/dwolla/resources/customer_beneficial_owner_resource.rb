91015de302596afa29d541b487087c38e2977245
Kevin Compton <klcompt@gmail.com>
2018-05-22 20:55:44 -0500

    Supporting certifying customer beneficial ownership info.

--- lib/money_mover/dwolla/resources/customer_beneficial_owner_resource.rb
+++ lib/money_mover/dwolla/resources/customer_beneficial_owner_resource.rb
@@ -3,6 +3,18 @@ module MoneyMover
     class CustomerBeneficialOwnerResource < BaseResource
       endpoint_path "/customers/:customer_id/beneficial-owners", action: [:list, :create]
       endpoint_path "/beneficial-owners/:id", action: [:update, :destroy, :find]
+
+      def certify_beneficial_ownership(id)
+        path = "/customers/#{id}/beneficial-ownership"
+
+        errors.clear
+        response = client.post path, { status: 'certified' }
+        unless response.success?
+          add_errors_from response
+        end
+
+        errors.empty?
+      end
     end
   end
 end
