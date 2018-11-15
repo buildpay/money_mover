7394120c38a42ae084414229a2678932f8a35383
Jonathan Dickert <jon.p.dickert@gmail.com>
2018-11-15 14:51:43 -0500

    debug stuff

--- lib/money_mover/dwolla/api_connection.rb
+++ lib/money_mover/dwolla/api_connection.rb
@@ -7,6 +7,7 @@ module MoneyMover
         @connection ||= Faraday.new(url: url_provider.api_url) do |faraday|
           faraday.authorization :Bearer, access_token if access_token
           faraday.headers[:accept] = "application/vnd.dwolla.v1.hal+json"
+          faraday.headers[:content_type] = "application/x-www-form-urlencoded"
           faraday.request :multipart
           faraday.request :json
           faraday.response :json, content_type: /\bjson$/
