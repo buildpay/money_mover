6bc2d9e9267fb9b96612df7c30c672b94e5b4e67
Jonathan Dickert <jon.p.dickert@gmail.com>
2018-11-15 14:27:51 -0500

    Might need multipart to be present

--- lib/money_mover/dwolla/api_connection.rb
+++ lib/money_mover/dwolla/api_connection.rb
@@ -8,6 +8,7 @@ module MoneyMover
           faraday.authorization :Bearer, access_token if access_token
           faraday.headers[:accept] = "application/vnd.dwolla.v1.hal+json"
           faraday.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+          faraday.request :multipart
           faraday.response :json, content_type: /\bjson$/
           faraday.adapter Faraday.default_adapter
         end
