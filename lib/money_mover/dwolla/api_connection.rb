1e7fdbc5cf597b8597953a87ebf4c41f84f13341
Jonathan Dickert <jon.p.dickert@gmail.com>
2018-11-15 14:25:00 -0500

    Might have set the right content type

--- lib/money_mover/dwolla/api_connection.rb
+++ lib/money_mover/dwolla/api_connection.rb
@@ -7,8 +7,7 @@ module MoneyMover
         @connection ||= Faraday.new(url: url_provider.api_url) do |faraday|
           faraday.authorization :Bearer, access_token if access_token
           faraday.headers[:accept] = "application/vnd.dwolla.v1.hal+json"
-          faraday.request :application
-          faraday.request :url_encoded
+          faraday.headers['Content-Type'] = 'application/x-www-form-urlencoded'
           faraday.response :json, content_type: /\bjson$/
           faraday.adapter Faraday.default_adapter
         end
