441027384f620ecc895fd5e59ef009cc1442d159
Jonathan Dickert <jon.p.dickert@gmail.com>
2018-11-15 14:06:02 -0500

    Changed request format

--- lib/money_mover/dwolla/api_connection.rb
+++ lib/money_mover/dwolla/api_connection.rb
@@ -8,7 +8,7 @@ module MoneyMover
           faraday.authorization :Bearer, access_token if access_token
           faraday.headers[:accept] = "application/vnd.dwolla.v1.hal+json"
           faraday.request :multipart
-          faraday.request :json
+          faraday.request "form-data"
           faraday.response :json, content_type: /\bjson$/
           faraday.adapter Faraday.default_adapter
         end
