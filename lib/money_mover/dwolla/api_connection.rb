00c65522d1b34781ce16f28e5376049f5fd9523c
leed <leed@gobuildpay.com>
2018-11-15 14:47:43 -0500

    adding url encoded content type manually in response

--- lib/money_mover/dwolla/api_connection.rb
+++ lib/money_mover/dwolla/api_connection.rb
@@ -7,11 +7,11 @@ module MoneyMover
         @connection ||= Faraday.new(url: url_provider.api_url) do |faraday|
           faraday.authorization :Bearer, access_token if access_token
           faraday.headers[:accept] = "application/vnd.dwolla.v1.hal+json"
-          faraday.headers['Content-Type'] = "application/x-www-form-urlencoded"
           faraday.request :multipart
           faraday.request :url_encoded
           faraday.request :json
           faraday.response :json, content_type: /\bjson$/
+          faraday.response :url_enconded, content_type: /\burl_encoded$/
           faraday.adapter Faraday.default_adapter
         end
       end
