d0d00f98f1cd3caab41aaa00a5f73c8f5d1770d4
leed <leed@gobuildpay.com>
2018-11-15 14:23:41 -0500

    adding url encoded content type

--- lib/money_mover/dwolla/api_connection.rb
+++ lib/money_mover/dwolla/api_connection.rb
@@ -8,6 +8,7 @@ module MoneyMover
           faraday.authorization :Bearer, access_token if access_token
           faraday.headers[:accept] = "application/vnd.dwolla.v1.hal+json"
           faraday.request :multipart
+          faraday.request :url_encoded
           faraday.request :json
           faraday.response :json, content_type: /\bjson$/
           faraday.adapter Faraday.default_adapter
