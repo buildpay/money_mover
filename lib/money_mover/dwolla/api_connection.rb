a3c2a7bc00a429407fa21ce63602997b90c5fb84
Jonathan Dickert <jon.p.dickert@gmail.com>
2018-11-15 14:20:25 -0500

    Switch back to url_encoded

--- lib/money_mover/dwolla/api_connection.rb
+++ lib/money_mover/dwolla/api_connection.rb
@@ -7,8 +7,8 @@ module MoneyMover
         @connection ||= Faraday.new(url: url_provider.api_url) do |faraday|
           faraday.authorization :Bearer, access_token if access_token
           faraday.headers[:accept] = "application/vnd.dwolla.v1.hal+json"
-          faraday.request :multipart
-          faraday.request :json
+          faraday.request :application
+          faraday.request :url_encoded
           faraday.response :json, content_type: /\bjson$/
           faraday.adapter Faraday.default_adapter
         end
