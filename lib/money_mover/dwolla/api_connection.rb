3ec8fc06ab462e106062fe3aa87bb9dea55303a3
leed <leed@gobuildpay.com>
2018-11-15 15:28:45 -0500

    modifying syntax for content type attr

--- lib/money_mover/dwolla/api_connection.rb
+++ lib/money_mover/dwolla/api_connection.rb
@@ -7,7 +7,7 @@ module MoneyMover
         @connection ||= Faraday.new(url: url_provider.api_url) do |faraday|
           faraday.authorization :Bearer, access_token if access_token
           faraday.headers[:accept] = "application/json"
-          faraday.headers[:content_type] = "application/www-form-urlencoded"
+          faraday.headers[:content_type] = "application/x-www-form-urlencoded"
           faraday.response :json, content_type: /\bjson$/
           faraday.adapter Faraday.default_adapter
         end
