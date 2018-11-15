6b3b8ee033effe7c843c62bbef0c66e135c9963b
Jonathan Dickert <jon.p.dickert@gmail.com>
2018-11-15 14:38:40 -0500

    Test to see if content type in header matters

--- lib/money_mover/dwolla/api_connection.rb
+++ lib/money_mover/dwolla/api_connection.rb
@@ -9,6 +9,7 @@ module MoneyMover
           faraday.headers[:accept] = "application/vnd.dwolla.v1.hal+json"
           faraday.headers['Content-Type'] = 'application/x-www-form-urlencoded'
           faraday.request :multipart
+          faraday.request :json
           faraday.response :json, content_type: /\bjson$/
           faraday.adapter Faraday.default_adapter
         end
