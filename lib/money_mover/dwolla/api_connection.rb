eccb166124818f5f8b8a88430935ab3e4790014b
Jonathan Dickert <jon.p.dickert@gmail.com>
2018-11-15 14:41:07 -0500

    Reset api connection to what it was

--- lib/money_mover/dwolla/api_connection.rb
+++ lib/money_mover/dwolla/api_connection.rb
@@ -7,7 +7,6 @@ module MoneyMover
         @connection ||= Faraday.new(url: url_provider.api_url) do |faraday|
           faraday.authorization :Bearer, access_token if access_token
           faraday.headers[:accept] = "application/vnd.dwolla.v1.hal+json"
-          faraday.headers['Content-Type'] = 'application/x-www-form-urlencoded'
           faraday.request :multipart
           faraday.request :json
           faraday.response :json, content_type: /\bjson$/
