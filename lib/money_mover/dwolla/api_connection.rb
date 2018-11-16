0fd64a5d4ec3ca6c1bdc65f91d1eac9dfeb084ab
Dustin <leed@dustin.home>
2018-11-16 09:10:50 -0500

    adding request params

--- lib/money_mover/dwolla/api_connection.rb
+++ lib/money_mover/dwolla/api_connection.rb
@@ -8,6 +8,8 @@ module MoneyMover
           faraday.authorization :Bearer, access_token if access_token
           faraday.headers[:accept] = "application/json"
           faraday.headers[:content_type] = "application/x-www-form-urlencoded"
+          faraday.response :multipart
+          faraday.response :url_encoded
           faraday.response :json, content_type: /\bjson$/
           faraday.adapter Faraday.default_adapter
         end
