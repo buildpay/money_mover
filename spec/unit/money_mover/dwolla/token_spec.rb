2eaa9fa60fccfdacebe8f01ff8d5e7f72dc399a7
leed <leed@gobuildpay.com>
2018-11-26 11:44:03 -0500

    WIP 4291 - refactored token spec

--- spec/unit/money_mover/dwolla/token_spec.rb
+++ spec/unit/money_mover/dwolla/token_spec.rb
@@ -21,6 +21,7 @@ end
 
 # INTEGRATION TEST
 describe MoneyMover::Dwolla::ApplicationToken do
+  let(:token) {subject.new}
   let(:refresh_token_request_params) { dwolla_helper.request_token_request_body }
 
   let(:account_id) { "7da912eb-5976-4e5c-b5ab-a5df35ac661b" }
@@ -44,41 +45,41 @@ describe MoneyMover::Dwolla::ApplicationToken do
     let(:content_type) { 'url_encoded' }
     let(:url_provider) { double 'url provider'}
 
-    let(:request_new_token!) { double 'request new token!' }
-    let(:new_token) { double 'new token', request_new_token!: request_new_token! }
-    let(:client) { double 'client', token_url: token_url}
+    let(:client) { double 'client', token_url: token_url, post: client_post}
+    let(:client_post) { client_response }
 
     let(:token_url) { dwolla_helper.get_token_url }
 
     let(:api_connection) { double 'api connection', connection: faraday_connection }
-    let(:faraday_connection) { double 'faraday connection' }
+    let(:faraday_connection) { double 'faraday connection', post: faraday_post }
+    let(:faraday_post) { server_request }
 
     let(:server_request) { double 'server request', response: token_response }
     let(:token_response) { refresh_token_success_response }
     let(:client_response) { double 'client response', body: token_response.to_json }
 
+    let(:new_token_instance ) { double 'new token instance' }
+
     let(:token) { nil }
 
     before do
+      # allow_any_instance_of(MoneyMover::Dwolla::Token).to receive(:request_new_token!) { new_token }
+
       allow(MoneyMover::Dwolla::Client).to receive(:new).with(content_type: content_type) { client }
       allow(MoneyMover::Dwolla::EnvironmentUrls).to receive(:new) { url_provider }
-      allow(MoneyMover::Dwolla::ApiConnection).to receive(:new).with(token, url_provider, content_type) { client_response }
-
-      allow(client).to receive(:post).with(client.token_url, refresh_token_request_params) { client_response }
-      allow(faraday_connection).to receive(:post).with(client.token_url, refresh_token_request_params) { server_request }
+      allow(MoneyMover::Dwolla::ApiConnection).to receive(:new).with(token, url_provider, content_type) { api_connection }
       allow(MoneyMover::Dwolla::ApiServerResponse).to receive(:new).with(server_request) { token_response }
-
-      allow(MoneyMover::Dwolla::Token).to receive(:new) { new_token }
+      allow(MoneyMover::Dwolla::Token).to receive(:new).with(client_response.body) { new_token_instance }
     end
 
     context 'success' do
       it 'updates token' do
         new_token = subject.request_new_token!
-        new_token_json_response = JSON.parse(client_response.body)
+        puts "#{new_token}"
 
-        expect(new_token_json_response["account_id"]).to eq(account_id)
-        expect(new_token_json_response["expires_in"]).to eq(3600)
-        expect(new_token_json_response["access_token"]).to eq(new_access_token)
+        expect(new_token.account_id).to eq(account_id)
+        expect(new_token.expires_in).to eq(3600)
+        expect(new_token.access_token).to eq(new_access_token)
       end
     end
   end
